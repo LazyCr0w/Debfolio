8df41a40e6f60c17a037cf5cd45ac838a0d273c4
LazyCr0w <sdfrosty777@gmail.com>
2025-09-12 19:17:46 +0530

    Added 2 new certificates

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -19,4 +19,20 @@ export const CERTIFICATIONS: Certification[] = [
     badgeImageURL: "/certification/oracle_badge.png",
     badgeURL: "https://catalog-education.oracle.com/pls/certview/sharebadge?id=89F051A978EF0C9FE1EE025E449D0E5D8B3303A17292B52E36987662B6222B57",
   },
+  {
+    title: "Introduction to Generative AI",
+    issuer: "Simpli Learn co-Powered with Google Cloud",
+    issuerLogoURL: "/certification/google.svg",
+    issueDate: "2025-09-12",
+    credentialID: "8962216",
+    credentialURL: "https://www.simplilearn.com/skillup-certificate-landing?token=eyJjb3Vyc2VfaWQiOiIzODA3IiwiY2VydGlmaWNhdGVfdXJsIjoiaHR0cHM6XC9cL2NlcnRpZmljYXRlcy5zaW1wbGljZG4ubmV0XC9zaGFyZVwvODk2MjIxNl85MzQ3NTA3MTc1NzY1OTk5NDQzNC5wbmciLCJ1c2VybmFtZSI6IlN1bW9uIERlYiJ9",
+  },
+  {
+    title: "Technology Job Simulation",
+    issuer: "Deloitte",
+    issuerLogoURL: "/certification/Deloitte.jpeg",
+    issueDate: "2025-09-12",
+    credentialID: "cB9EWgKcdoLnJYT9m",
+    credentialURL: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/9PBTqmSxAf6zZTseP/udmxiyHeqYQLkTPvf_9PBTqmSxAf6zZTseP_68c3febe81edf82e828a5fb1_1757681194116_completion_certificate.pdf",
+  },
 ];
